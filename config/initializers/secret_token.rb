cd52872f8baca86104756d80fc9d1b4473babf38
Salem Abei <abei.salem@gmail.com>
2014-01-25 11:34:44 +0100

    Erstes Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Streetdiary3::Application.config.secret_key_base = 'eff18a6462e0baf2c433804f2c85f16b8c7d0585836cbf76018992dabf149244ddcafdd82bdbb9747f1cd82f4e9c2a3ca82d0f7fd10b1d086d1ec3ca207379e2'
